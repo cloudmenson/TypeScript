7e67931935cfd5c8de54963eb15f404c0f8a48ba
Danylo <cloudmenson9@gmail.com>
2023-01-21 17:27:24 +0200

    [MainTypes] - Basics

---
+++ Main types/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let revenue = 500.2;
+let bonus = 200;
+let sayHello = "Hello TypeScript!";
+let learningTypeScript = true;
+let result = revenue + bonus;
+console.log(result);
---
+++ Main types/app.ts
@@ -0,0 +1,7 @@
+let revenue: number = 500.2;
+let bonus: number = 200;
+let sayHello: string = "Hello TypeScript!";
+let learningTypeScript: boolean = true;
+
+let result: number = revenue + bonus;
+console.log(result);
